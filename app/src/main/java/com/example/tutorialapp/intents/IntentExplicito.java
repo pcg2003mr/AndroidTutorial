e0ed543d63b7b3c6d106f3b15d81c6b803cc1b3f
pcg2003mr <pcg2003mr@gmail.com>
2024-10-03 13:09:13 +0200

    Añadimos nuevos comandos y paginas de layout

--- app/src/main/java/com/example/tutorialapp/intents/IntentExplicito.java
+++ app/src/main/java/com/example/tutorialapp/intents/IntentExplicito.java
@@ -1,6 +1,10 @@
 package com.example.tutorialapp.intents;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +25,24 @@ public class IntentExplicito extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnComprar = findViewById(R.id.BotonActividad);
+        EditText editTextComida = findViewById(R.id.editTextComida);
+        EditText editTextBebida = findViewById(R.id.editTextBebida);
+
+        btnComprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(IntentExplicito.this, IntentImplicito.class);
+
+                i.putExtra("Prueba", "Hamburguesas");
+                i.putExtra("Comida", editTextComida.getText().toString());
+                i.putExtra("Bebida", editTextBebida.getText().toString());
+
+                startActivity(i);
+            }
         });
     }
 }
--- app/src/main/java/com/example/tutorialapp/intents/IntentImplicito.java
+++ app/src/main/java/com/example/tutorialapp/intents/IntentImplicito.java
@@ -1,6 +1,7 @@
 package com.example.tutorialapp.intents;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +23,16 @@ public class IntentImplicito extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Bundle extras = getIntent().getExtras();
+
+        String textoPrueba = extras.getString("Prueba");
+        String comida = extras.getString("Comida");
+        String bebida = extras.getString("Bebida");
+
+
+
+
+        Log.d("Prueba", textoPrueba);
     }
 }
